c82dbe4e632a46390100e3c51e20951c6dd01118
A Aziz <aziz2743@hotmail.com>
2023-05-13 07:39:42 +0700

    revise with times iteration interval

--- src/listener.ts
+++ src/listener.ts
@@ -1,31 +1,23 @@
 import { connect, StringCodec } from "nats";
 const servers = process.env.NATS_URL || "nats://localhost:4222";
+var i = 0;
+const do_listen = () => {
+  connect({
+    servers: servers.split(','),
+  }).then(async (nc) => {
+    console.clear();
+    console.log('Successfully connect, iteration:', i++);
+    const sc = StringCodec();
+    const sub = nc.subscribe("greet.*", { max: 100 });
+    for await (const msg of sub) {
+      console.log(`${sc.decode(msg.data)} on subject ${msg.subject}`);
+    }
+    nc.drain();
+  }).catch((error) => {
+    console.log(error)
+  });
+};
 
-// const listener = async () => {
-//   const nc = await connect({
-//     servers: servers.split(","),
-//   });
-//   const sc = StringCodec();
-//   let sub = nc.subscribe("greet.*", { max: 3 });
-//   for await (const msg of sub) {
-//     console.log(`${sc.decode(msg.data)} on subject ${msg.subject}`);
-//   }
-//   await nc.drain();
-
-// }
-
-// listener();
-connect({
-  servers: servers.split(','),
-}).then(async (nc) => {
-  console.log('Successfully connect')
-  const sc = StringCodec();
-  const sub = nc.subscribe("greet.*", { max: 3 });
-  for await (const msg of sub) {
-    console.log(`${sc.decode(msg.data)} on subject ${msg.subject}`);
-  }
-  // nc.drain();
-}).catch((error) => {
-  console.log(error)
-});
-
+setInterval(() => {
+  do_listen();
+}, 5000);
--- src/publisher.ts
+++ src/publisher.ts
@@ -1,18 +1,24 @@
 import { connect, StringCodec } from 'nats';
 
 const servers = process.env.NATS_URL || 'nats://localhost:4222';
+var i = 0;
+const do_pub = () => {
+  connect({
+    servers: servers.split(','),
+  }).then((nc) => {
+    console.clear();
+    console.log('Successfully connect, iteration: ', i++)
+    const sc = StringCodec();
+    nc.publish('greet.bob', sc.encode('hello-' + i));
+    nc.publish("greet.joe", sc.encode("hello"));
+    nc.publish("greet.pam", sc.encode("hello"));
+    nc.publish("greet.sue", sc.encode("hello"));
+    nc.drain();
+  }).catch((error) => {
+    console.log(error)
+  });
+};
 
-connect({
-  servers: servers.split(','),
-}).then((nc) => {
-  console.log('Successfully connect')
-  const sc = StringCodec();
-  nc.publish('greet.bob', sc.encode('hello'));
-  nc.publish("greet.joe", sc.encode("hello"));
-  nc.publish("greet.pam", sc.encode("hello"));
-  nc.publish("greet.sue", sc.encode("hello"));
-
-
-}).catch((error) => {
-  console.log(error)
-});
+setInterval(() => {
+  do_pub();
+}, 2000);
